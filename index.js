fe8ac546b261c2d93b83d40ef9e6d4e4725d1303
emidwate <matthewcode04@gmail.com>
2024-10-16 14:33:11 +0200

    Navbar background

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const hamMenu = document.querySelector(".nav-ham-menu")
 const offScreenMenu = document.querySelector(".off-screen-menu")
 const addressElem = document.querySelector(".hero-subsection-address")
-const nav = document.querySelector(".nav")
-
-const navClone = nav.cloneNode(true)
 
 hamMenu.addEventListener("click", () => {
     offScreenMenu.classList.toggle("active")
@@ -13,20 +10,20 @@ hamMenu.addEventListener("click", () => {
     }
 })
 
+document.addEventListener("scroll", changeNav)
+
 function closeMenu(e) {
   if (!hamMenu.contains(e.target) && !offScreenMenu.contains(e.target)) {
     offScreenMenu.classList.remove("active")
   }
 }
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      nav.style.backgroundColor = "white"
-    } else {
-      nav.style.backgroundColor = "transparent"
-    }
-  })
-})
+function changeNav() {
+  const nav = document.querySelector(".nav")
 
-observer.observe(addressElem)
+  if (window.scrollY == 0 ) {
+    nav.style.backgroundColor = "transparent"
+  } else {
+    nav.style.backgroundColor = "white"
+  }
+}
